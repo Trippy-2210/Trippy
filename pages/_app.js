f6d736a7f8b43f3b37cfda994b40b9d5cc70e91a
Aaron Mikulka <aaronjmikulka@gmail.com>
2023-01-16 15:15:21 -0700

    removes unnecessary css styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import "../styles/styles.css";
 export default function App({ Component, pageProps }) {
   return (
     <>
--- pages/trips/addtrip.js
+++ pages/trips/addtrip.js
@@ -1,25 +1,24 @@
 import { useState } from "react";
 import PlaceAutocomplete from "../../components/trips/PlaceAutocomplete";
-import styles from "../../styles/addtrip.module.css";
 
 export default function Addtrip() {
   const [value, setValue] = useState(null);
   return (
     <div>
-      <div className={styles.searchbar + " add-trip-custom-search"}>
+      <div>
         <PlaceAutocomplete value={value} setValue={setValue} />
       </div>
     </div>
   );
 }
 
-const values = {
-  description: "Santa Fe, NM, USA",
-  matched_substrings: "[{…}]",
-  place_id: "ChIJqVKY50NQGIcRQN-I_XMjkIw",
-  reference: "ChIJqVKY50NQGIcRQN-I_XMjkIw",
-  structured_formatting:
-    '{main_text: "Santa Fe", main_text_matched_substring…}',
-  terms: "[{…}, {…}, {…}]",
-  types: '["locality", "political", "geocode"]',
-};
+// const values = {
+//   description: "Santa Fe, NM, USA",
+//   matched_substrings: "[{…}]",
+//   place_id: "ChIJqVKY50NQGIcRQN-I_XMjkIw",
+//   reference: "ChIJqVKY50NQGIcRQN-I_XMjkIw",
+//   structured_formatting:
+//     '{main_text: "Santa Fe", main_text_matched_substring…}',
+//   terms: "[{…}, {…}, {…}]",
+//   types: '["locality", "political", "geocode"]',
+// };
