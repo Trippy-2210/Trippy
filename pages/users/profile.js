556213ef2031d24ac690917e9039282d0e420b8f
Haillie W <hailliewilloughby@gmail.com>
2023-01-17 03:53:59 -0500

    Create profile page with info and trips sections

---
+++ pages/users/profile.js
@@ -0,0 +1,13 @@
+import UserInfo from '../../components/users/profile/UserInfo.js'
+import UserTripList from '../../components/users/profile/UserTripList.js'
+
+const Profile = () => {
+  return (
+    <>
+      <UserInfo />
+      <UserTripList />
+    </>
+  )
+}
+
+export default Profile;
