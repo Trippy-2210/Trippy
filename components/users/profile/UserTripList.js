bf5c04b61d5cb5bb8441b2cda79d90188ab5fbee
Haillie W <hailliewilloughby@gmail.com>
2023-01-18 16:07:45 -0500

    Add/comment out trip route

--- components/users/profile/UserTripList.js
+++ components/users/profile/UserTripList.js
@@ -1,33 +1,28 @@
-import useSWR from 'swr'
-// import { useRouter } from 'next/router'
-import UserTripListItem from './UserTripListItem.js'
+// import useSWR from 'swr'
+// // import { useRouter } from 'next/router'
+// import UserTripListItem from './UserTripListItem.js'
 
-const fetcher = async (url) => {
-  const res = await axios.get(url);
-  return res.data;
-}
+// const fetcher = async (url) => {
+//   const res = await axios.get(url);
+//   return res.data;
+// }
 
-const UserTrips = ({ ownerId }) => {
-  // let router = useRouter();
-  // let {id} = router.query;
+// const UserTrips = ({ ownerId }) => {
+//   // let router = useRouter();
+//   // let {id} = router.query;
 
-  const { data, error, isLoading } = useSWR(`/api/trips`, fetcher)
+//   const { data, error, isLoading } = useSWR(`/api/trips/${ownerId}`, fetcher)
 
-  if (isLoading) return <h2>Loading...</h2>
-  if (error) return <h2>Failed to load trips</h2>
+//   if (isLoading) return <h2>Loading...</h2>
+//   if (error) return <h2>Failed to load trips</h2>
 
-  return (
-    <>
-      <div className="user-trips">
-        {data.map((trip, i) => {
-          if (trip.ownerId === ownerId) {
-            console.log('trip owner id:', trip.ownerId);
-            return <UserTripListItem key={i} trip={trip}/>
-          }
-        })}
-      </div>
-    </>
-  )
-}
+//   return (
+//     <>
+//       <div className="user-trips">
+//         {data.map((trip, i) =>  <UserTripListItem key={i} trip={trip}/>)}
+//       </div>
+//     </>
+//   )
+// }
 
-export default UserTrips;
+// export default UserTrips;
