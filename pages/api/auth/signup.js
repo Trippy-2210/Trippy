13ddc72645fdf8eea3a87fb79604a5b8d15f6f13
Julian McLendon <jkmclendon@gmail.com>
2023-01-17 19:24:31 -0500

    Auth stuff

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -9,7 +9,6 @@ export default async function handler(req, res) {
   const salt = crypto.createHash('sha256').update(token).digest('hex');
   let hash = crypto.createHash('sha256').update(pw + salt).digest('hex');
   let session_id = req.headers.session_id;
-  console.log(session_id);
 
   let id = await sql`INSERT INTO users (email, password, salt) VALUES (${email}, ${hash}, ${salt}) RETURNING id;`;
   await sql`UPDATE sessions SET user_id = ${id[0].id} WHERE session_id = ${session_id};`;
