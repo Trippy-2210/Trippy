662a372b4cbdf97861ed7b895a85aead6d4bdc1e
Aaron Mikulka <aaronjmikulka@gmail.com>
2023-01-18 08:25:49 -0700

    adds activity list

--- components/trips/TextInput.js
+++ components/trips/TextInput.js
@@ -10,7 +10,7 @@ export default function TextInput({ label, value, setValue }) {
       value={value}
       label={label}
       onChange={handleChange}
-      variant='standard'
+      variant='outlined'
     />
   );
 }
