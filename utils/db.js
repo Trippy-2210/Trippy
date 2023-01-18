9cc0b2b4347ff7c1a55091f24f203148b6bf490c
Aaron Mikulka <aaronjmikulka@gmail.com>
2023-01-18 13:09:58 -0700

    changes schema, starts api to add trip

--- utils/db.js
+++ utils/db.js
@@ -13,13 +13,18 @@ const options = { useNewUrlParser: true, useUnifiedTopology: true };
 mongoose.set('strictQuery', true);
 mongoose.connect(url, options);
 
-const UserData = new mongoose.model('UserData', userDataSchema);
-const Session = new mongoose.model('Session', sessionSchema);
-
-const Profile = new mongoose.model('Profile', profileSchema);
-const Trip = new mongoose.model('Trip', tripSchema);
-const Message = new mongoose.model('Message', messageSchema);
-const Notification = new mongoose.model('Notification', notificationSchema);
+const UserData =
+  mongoose.models.UserData || new mongoose.model('UserData', userDataSchema);
+const Session =
+  mongoose.models.Session || new mongoose.model('Session', sessionSchema);
+const Profile =
+  mongoose.models.Profile || new mongoose.model('Profile', profileSchema);
+const Trip = mongoose.models.Trip || new mongoose.model('Trip', tripSchema);
+const Message =
+  mongoose.models.Message || new mongoose.model('Message', messageSchema);
+const Notification =
+  mongoose.models.Notification ||
+  new mongoose.model('Notification', notificationSchema);
 
 var schemas = {
   UserData,
