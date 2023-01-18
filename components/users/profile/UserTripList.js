66eaa5aab83af1653a5b494f8f94d31de6cd9018
Haillie W <hailliewilloughby@gmail.com>
2023-01-18 11:15:04 -0500

    Comment out file

--- components/users/profile/UserTripList.js
+++ components/users/profile/UserTripList.js
@@ -1,13 +1,13 @@
-import UserTripListItem from './UserTripListItem.js'
+// import UserTripListItem from './UserTripListItem.js'
 
-const UserTrips = ({ trips }) => {
-  return (
-    <>
-      <div className="user-trips">
-        {trips.map((trip, i) => <UserTripListItem key={i} trip={trip}/>)}
-      </div>
-    </>
-  )
-}
+// const UserTrips = ({ trips }) => {
+//   return (
+//     <>
+//       <div className="user-trips">
+//         {trips.map((trip, i) => <UserTripListItem key={i} trip={trip}/>)}
+//       </div>
+//     </>
+//   )
+// }
 
-export default UserTrips;
+// export default UserTrips;
