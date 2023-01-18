f88960ac16806bfa64d68bea28affc4d7fa79b33
Haillie W <hailliewilloughby@gmail.com>
2023-01-18 10:54:50 -0500

    Add swr daya fetching

--- pages/api/profiles/[id].js
+++ pages/api/profiles/[id].js
@@ -3,8 +3,8 @@ import schemas from '../../../utils/db.js';
 export default async function handler(req, res) {
   const profile = await schemas.Profile.findById(req.query.id);
   console.log(`***** profile: ${profile} *****`)
-  if (profile == true) {
-    res.status(200).text(profile);
+  if (profile !== null) {
+    res.status(200).json(profile);
   } else {
     res.status(404).json({message: `Profile of user-id: ${req.query.id} not found`})
   }
--- pages/users/profile/[id]/index.js
+++ pages/users/profile/[id]/index.js
@@ -1,39 +1,69 @@
-import UserInfo from '../../components/users/profile/UserInfo.js'
-import UserTripList from '../../components/users/profile/UserTripList.js'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import useSWR from 'swr'
+import UserInfo from '/home/itsnothalley/hackreactor/blue-ocean/Trippy/components/users/profile/UserInfo.js'
+import UserTripList from '/home/itsnothalley/hackreactor/blue-ocean/Trippy/components/users/profile/UserTripList.js'
 
+const fetcher = async (url) => {
+  const res = await axios.get(url);
+  return res.data;
+}
 
-const Profile = ({ profile }) => {
+const Profile = () => {
+  let router = useRouter();
+  let {id} = router.query;
+
+  const { data, error, isLoading } = useSWR(`/api/profiles/${id}`, fetcher)
+
+  // const fetcher = url => axios.get(url).then(res => res.data)
+
+  // function useUser ({id}) {
+  //   const { data, error, isLoading } = useSWR(`/api/profiles/${id}`, fetcher)
+  //   return {
+  //     user: data,
+  //     isLoading,
+  //     isError: error
+  //   }
+  // }
+
+  // const { data, isLoading, isError } = useUser({id})
+
+  if (isLoading) return <h2>Loading...</h2>
+  if (error) return <h2>Failed to load</h2>
+
   return (
     <>
-      <UserInfo firstName={profile.firstName} lastName={profile.lastName} bio={profile.bio} photo={profile.photo} />
+      <UserInfo firstName={data.firstName} lastName={data.lastName} bio={data.bio} photo={data.photo} />
       <UserTripList />
     </>
   )
 }
 
-export const getStaticProps = async (context) => {
-  const res = await fetch(`http://localhost:3000/api/profiles/${context.params.id}`)
-  const profile = await res.json()
-  return {
-    props: {
-      profile
-    }
-  }
-}
 
-export const getStaticPaths = async () => {
-  const res = await fetch(`http://localhost:3000/api/profiles`)
 
-  const profiles = await res.json() // getting error
+// export const getStaticProps = async (context) => {
+//   const res = await fetch(`http://localhost:3000/api/profiles/${context.params.id}`)
+//   const profile = await res.json()
+//   return {
+//     props: {
+//       profile
+//     }
+//   }
+// }
 
-  const ids = profiles.map((profile) => profile._id)
+// export const getStaticPaths = async () => {
+//   const res = await fetch(`http://localhost:3000/api/profiles`)
 
-  const paths = ids.map((id) => ({params: {id: id.toString()}}))
+//   const profiles = await res.json() // getting error
 
-  return {
-    paths,
-    fallback: false
-  }
-}
+//   const ids = profiles.map((profile) => profile._id)
+
+//   const paths = ids.map((id) => ({params: {id: id.toString()}}))
+
+//   return {
+//     paths,
+//     fallback: false
+//   }
+// }
 
 export default Profile;
