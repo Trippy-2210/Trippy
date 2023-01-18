4ac7a1613a92294ed1664c57b0fada6436f8f6d6
Haillie W <hailliewilloughby@gmail.com>
2023-01-18 11:17:11 -0500

    Remove comments

--- pages/users/profile/[id]/index.js
+++ pages/users/profile/[id]/index.js
@@ -15,55 +15,15 @@ const Profile = () => {
 
   const { data, error, isLoading } = useSWR(`/api/profiles/${id}`, fetcher)
 
-  // const fetcher = url => axios.get(url).then(res => res.data)
-
-  // function useUser ({id}) {
-  //   const { data, error, isLoading } = useSWR(`/api/profiles/${id}`, fetcher)
-  //   return {
-  //     user: data,
-  //     isLoading,
-  //     isError: error
-  //   }
-  // }
-
-  // const { data, isLoading, isError } = useUser({id})
-
   if (isLoading) return <h2>Loading...</h2>
   if (error) return <h2>Failed to load</h2>
 
   return (
-    <>
+    <div>
       <UserInfo firstName={data.firstName} lastName={data.lastName} bio={data.bio} photo={data.photo} />
-      <UserTripList />
-    </>
+      {/* <UserTripList /> */}
+    </div>
   )
 }
-
-
-
-// export const getStaticProps = async (context) => {
-//   const res = await fetch(`http://localhost:3000/api/profiles/${context.params.id}`)
-//   const profile = await res.json()
-//   return {
-//     props: {
-//       profile
-//     }
-//   }
-// }
-
-// export const getStaticPaths = async () => {
-//   const res = await fetch(`http://localhost:3000/api/profiles`)
-
-//   const profiles = await res.json() // getting error
-
-//   const ids = profiles.map((profile) => profile._id)
-
-//   const paths = ids.map((id) => ({params: {id: id.toString()}}))
-
-//   return {
-//     paths,
-//     fallback: false
-//   }
-// }
 
 export default Profile;
