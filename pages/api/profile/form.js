1c2c24a9df7d476cf003e8530e44e9aa807c6e80
Haillie W <hailliewilloughby@gmail.com>
2023-01-19 06:14:43 -0500

    Add post to database code

--- pages/api/profile/form.js
+++ pages/api/profile/form.js
@@ -1,25 +1,17 @@
-// import schemas from '../../../utils/db.js';
+import schemas from '../../../utils/db.js';
 
-const handler = (req, res) => {
-  // Get data submitted in request's body.
+export default async function handler(req, res) {
   const body = req.body
 
-  // Optional logging to see the responses
-  // in the command line where next.js app is running.
-  // console.log('body: ', body)
-
-  // Guard clause checks for first and last name,
-  // and returns early if they are not found
   if (!body.firstName || !body.lastName) {
-    // Sends a HTTP bad request error code
     return res.status(400).json({ data: 'First or last name not found' })
   }
-
-
-
-  // Found the name.
-  // Sends a HTTP success code
+  await schemas.Profile.create({
+    userId: '',
+    firstName: body.firstName,
+    lastName: body.lastName,
+    bio: body.bio,
+    photo: ''
+  })
   res.status(200).json({ data: body })
 }
-
-export default handler
