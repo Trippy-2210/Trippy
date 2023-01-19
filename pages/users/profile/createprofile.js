a2d31d7f31e4d8176bdbafc0c32f6b16f9db3aba
Haillie W <hailliewilloughby@gmail.com>
2023-01-19 06:42:20 -0500

    Remove cloudinary uplaod widget

--- pages/users/profile/createprofile.js
+++ pages/users/profile/createprofile.js
@@ -1,13 +1,11 @@
 import ProfilePhoto from '../../../components/users/createprofile/ProfilePhoto.js'
 import UserInfo from '../../../components/users/createprofile/UserInfo.js'
-import CloudinaryWidget from '../../../components/trips/CloudinaryUpload.js'
 
 const CreateProfile = () => {
   return (
     <>
       <h1>Create Profile</h1>
       <div className="create-profile-section">
-        <CloudinaryWidget label={'Upload Profile Photo'} />
         <UserInfo />
       </div>
     </>
