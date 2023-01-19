f102c75dfc962c4bf959b7b56e8374c15741af70
Julian McLendon <jkmclendon@gmail.com>
2023-01-19 17:06:55 -0500

    import msg styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globalStyles.css';
 import { ThemeProvider } from '@mui/material';
 import { theme } from '../styles/styles.js';
+import '../styles/messages.css';
 
 export default function App({ Component, pageProps }) {
   return (
