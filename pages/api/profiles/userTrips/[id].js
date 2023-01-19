d16eb7c389df20b742be42b3d898f19cf1b86526
Haillie W <hailliewilloughby@gmail.com>
2023-01-18 21:49:08 -0500

    Comment out log

--- pages/api/profiles/userTrips/[id].js
+++ pages/api/profiles/userTrips/[id].js
@@ -1,11 +1,11 @@
-import schemas from '../../../utils/db.js';
+import schemas from '../../../../utils/db.js';
 
 export default async function handler(req, res) {
-  const trip = await schemas.Trip.find({ownerId: req.query.id});
-  console.log(`***** trip: ${trip} *****`)
-  if (trip !== null) {
-    res.status(200).json(trip);
+  const trips = await schemas.Trip.find({ownerId: req.query.id});
+  // console.log(`***** trips: ${trips} *****`)
+  if (trips !== null) {
+    res.status(200).json(trips);
   } else {
-    res.status(404).json({message: `Trip of user-id: ${req.query.id} not found`})
+    res.status(404).json({message: `Trips of user-id: ${req.query.id} not found`})
   }
 }
