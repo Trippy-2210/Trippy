d446eb718dbad956407efba80199dca2b3d345fa
Haillie W <hailliewilloughby@gmail.com>
2023-01-19 11:59:54 -0500

    Add photo url and req header userid to db query

--- pages/api/profile/form.js
+++ pages/api/profile/form.js
@@ -1,17 +1,17 @@
 import schemas from '../../../utils/db.js';
 
 export default async function handler(req, res) {
-  const body = req.body
+  const body = req.body;
 
   if (!body.firstName || !body.lastName) {
     return res.status(400).json({ data: 'First or last name not found' })
   }
   await schemas.Profile.create({
-    userId: '',
+    userId: req.headers.userid,
     firstName: body.firstName,
     lastName: body.lastName,
     bio: body.bio,
-    photo: ''
+    photo: body.photo
   })
   res.status(200).json({ data: body })
 }
