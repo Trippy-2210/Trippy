b3b4daa494e29eb02e4063549ff425fc150362d1
Stephen Chong <stephenc326@gmail.com>
2023-01-20 10:30:12 -0500

    Deleted comments

---
+++ pages/messages/createPoll.js
@@ -0,0 +1,131 @@
+import {useState, useEffect} from 'react';
+import Link from 'next/link'
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Input from '@mui/material/Input';
+import Stack from '@mui/material/Stack';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
+import Alert from '@mui/material/Alert';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+
+const CreatePolls = () => {
+  const [title, setTitle] = useState("Create a question here");
+  const [options, setOptions] = useState(["Option 1", "Option 2"]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const submit = () => {
+    setError("");
+
+    // Validate title
+    if (title.length < 5 || title.length > 60) {
+      setError("Title must be between 5 and 100 characters");
+      return;
+    }
+
+    // Validate options
+    if (options.some((option) => option.length < 1 || option.length > 60)) {
+      setError("Options must be between 1 and 60 characters");
+      return;
+    }
+
+
+  };
+
+  return (
+    <div>
+      <Container maxW="container.lg">
+
+
+      <Container maxW="container.md" mt={6} shadow="lg" p={8} rounded="2xl">
+        <h1>Create a poll</h1>
+
+        <Input
+          placeholder="Poll Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+
+        <Stack spacing={4}>
+          {options.map((option, index) => (
+            <Item key={index}>
+
+                <Input
+                  placeholder="Option"
+                  value={option}
+                  onChange={(e) => {
+                    const newOptions = [...options];
+                    newOptions[index] = e.target.value;
+                    setOptions(newOptions);
+                  }}
+                />
+
+                <Stack direction="row" ml={2}>
+                  <Button
+                    onClick={() => {
+                      const newOptions = [...options];
+                      newOptions.splice(index, 1);
+                      setOptions(newOptions);
+                    }}
+                    disabled={options.length <= 2}
+                    colorScheme="red"
+                    variant="ghost"
+                  >
+                    <DeleteIcon />
+                  </Button>
+
+                  <Button
+                    onClick={() => {
+                      const newOptions = [...options];
+                      newOptions.push("");
+                      setOptions(newOptions);
+                    }}
+                    disabled={
+                      options.length >= 10 || index !== options.length - 1
+                    }
+                    colorScheme="green"
+                  >
+                    <AddIcon />
+                  </Button>
+                </Stack>
+
+            </Item>
+          ))}
+        </Stack>
+
+        {error && (
+          <Alert mt={4} status="error">
+            {error}
+          </Alert>
+        )}
+        <Button variant="contained">
+        <Link href="/messages/viewPoll" onClick={submit}>Create Poll</Link>
+        </Button>
+
+        <Button variant="contained">
+      <Link href="/messages/index">Back to messages</Link>
+      </Button>
+      </Container>
+    </Container>
+    </div>
+  );
+};
+
+export default CreatePolls;
